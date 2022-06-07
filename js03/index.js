393219592db28e634adf28b2fd1b1c9e0c3b8792
Adriele Maria <adrieleifrn@gmail.com>
2022-06-07 08:36:33 -0300

    adicionando pasta js03

---
+++ js03/index.js
@@ -0,0 +1,41 @@
+
+let numero1 = parseFloat(document.getElementById('num1').value);
+let numero2 = parseFloat(document.getElementById('num2').value);
+
+let escolha = document.querySelector('#calc').value;
+
+const meuBotao = document.getElementById('btn');
+
+let h3 = document.getElementById('h3');
+
+
+meuBotao.addEventListener('click', () => {
+    let resultado;
+
+    switch(escolha){
+        case "soma":
+            resultado = numero1 + numero2;
+            h3.innerHTML = resultado;
+            break;
+
+        case "subt":
+            resultado = numero1 - numero2;
+            h3.innerHTML = resultado;
+            break;
+
+        case "mult":
+            resultado = numero1 * numero2;
+            h3.innerHTML = resultado;
+            break;
+
+        case "divi":
+            resultado = numero1 / numero2;
+            h3.innerHTML = resultado;
+            break;
+
+        default :
+            console.log("Opção inválida");
+            break;
+    }
+    
+})
