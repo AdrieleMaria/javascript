0fcec6f275d7cc0eb8c9d10107b09530b50c6e77
Adriele Maria <adrieleifrn@gmail.com>
2022-06-26 02:16:35 -0300

    atualizando a pasta js08

--- js08/controllers/letraE.js
+++ js08/controllers/letraE.js
@@ -4,8 +4,10 @@
 
 
 
-const obj={};
+let obj={};
 
 obj =  {'Um atributo com espaco': 1};
 
-console.log(['Um atributo com espaco'], Object.values(obj)[0]);
+console.log(obj['Um atributo com espaco']);
+
+console.log(Object.values(obj)[0]);
