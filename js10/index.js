64ebbb6b9db5d9255cc75268c04838d2cf03fbc0
megatron0000 <vitor_pimenta97@hotmail.com>
2022-07-17 20:39:40 -0400

    Correção

--- js10/index.js
+++ js10/index.js
@@ -1,3 +1,13 @@
+/**
+ * Entre essas variáveis (_id até incluidoEm),
+ * somente o _id precisa ser global,
+ * as outras poderiam ser variáveis locais dentro
+ * da/das função/funções que as utilizam.
+ * 
+ * Motivo: variável global é geralmente um problema, porque
+ * elas dificultam raciocinar sobre o código (guardam um estado
+ * que pode ser alterado em qualquer lugar do código)
+ */
 // Inicialização das variáveis do sistema
 let _id = 0;
 let id;
@@ -6,15 +16,21 @@ let descricao = '';
 let valor = 0.0;
 let incluidoEm = new Date();
 
-let produto = {};
+let produto = {}; // poderia ser variável local na função que a utiliza
 let produtos = []; //ARRAY DE TIPO PRODUTO
 
+// variável não utilizada
 let produtoDetalhe = { msg: 'nada ainda' }; //inicializa/carrega os dados para editar e excluir
 
 /**Função Listar tabela dinâmica */
-let tRowEl = [];
-let trProduto = ``;
-let tRow = document.querySelector('tr');
+let tRowEl = []; // podia ser uma variável local dentro da função que a utiliza
+let trProduto = ``; // podia ser uma variável local dentro da função que a utiliza
+let tRow = document.querySelector('tr'); // não é utilizada
+
+/**
+ * Semântica: como essas 3 variáveis não mudam,
+ * elas poderiam ser "const"
+ */
 let tBody = document.querySelector('tbody');
 let btnAtualizar = document.querySelector('#btn-atualizar');
 let btnIncluir = document.querySelector('#btn-incluir');
@@ -23,6 +39,19 @@ let btnIncluir = document.querySelector('#btn-incluir');
 
 iniciarDadosMock(); //Função que popula o Array de produtos com dados iniciais
 
+
+/**
+ * Entendi o propósito desse event listener,
+ * mas acho que foi uma complicação desnecessária.
+ * 
+ * Ele está aqui para achar qual foi o id clicado
+ * (porque ele reage a qualquer clique dentro da tabela,
+ * então você tem que descobrir qual foi o <tr> clicado)
+ * 
+ * Outra maneira de implementar que me parece mais fácil:
+ * ao criar os <tr>, coloque um "onclick" neles (passando o id
+ * como parâmetro pra função). Assim você sempre terá o id correto
+ */
 let target = '';
 tBody.addEventListener('click', function (evento) {
   evento.preventDefault();
@@ -66,6 +95,12 @@ tBody.addEventListener('click', function (evento) {
 
 function detalheProdutoDisplay(obj) {
   //.innerhtml
+  /* 
+    Será que precisa invocar listarTabelaDinamica() aqui ?
+    Qual seria a ideia ? 
+
+    Vi seu comentário "Reobter a referência dos objetos" mas não entendi
+  */
   listarTabelaDinamica(); //Reobter a referência dos objetos no componente da tabela
   console.log('detalheProdutoDisplay:', obj);
   document.querySelector('.id').innerHTML = obj.id;
@@ -118,12 +153,18 @@ function listarTabelaDinamica() {
   }
 }
 document.querySelector('.atualiza').addEventListener('click', () => {
+  // por que não colocar esses comandos dentro da função atualizarProduto() ?
+  // Se estivesse lá, também funcionaria...
   btnAtualizar.style.display = 'none';
   btnIncluir.style.display = 'block';
 });
 
 function atualizarProduto() {
   //Reatribui as novas informações do produto
+  /*
+    produtos[id] nem sempre funciona, veja comentário
+    da função editar()
+  */
   produtos[id].nome = document.querySelector('#nome').value;
   produtos[id].descricao = document.querySelector('#descricao').value;
   produtos[id].valor = document.querySelector('#valor').value;
@@ -148,6 +189,18 @@ function editar(indice) {
   document.querySelector('#descricao').value = objTmp.descricao;
   document.querySelector('#valor').value = objTmp.valor;
 
+  /* 
+    Isso não funciona sempre !
+    Na função atualizarProduto(), você usa produtos[id]
+    mas id não é sempre o mesmo que o índice. Você deveria
+    usar produtos[indice] lá. indice deveria ser variável global
+
+    Teste rápido pra ver que pode dar errado:
+    1. abra a página
+    2. apague o produto do meio
+    3. edite o último produto (que continua com id=2 mas agora tem índice=1)
+    4. vai dar erro =)
+  */
   id = objTmp.id;
   //console.log('editar: ', produtos[id]);
 }
@@ -162,12 +215,34 @@ function excluir(indice) {
   if (produtos.length == 1) {
     produtos = [];
   } else {
-    produtos[indice] = produtos[tamArr]; //recebe o último elemento do array
-    //Rotina que cria outro array de objetos tipo produtos
+    /**
+     * Essa lógica está "errada" no sentido de que ela
+     * troca a ordem original dos produtos na lista.
+     * 
+     * Teste rápido para entender do que estou falando:
+     * 1. abra a página
+     * 2. delete o Alicate
+     * 3. Verá que agora a Cadeira Presidente aparece antes de Panela de Pressão
+     * 
+     * Isso não está errado, só é meio inesperado do ponto de vista do cliente
+     */
+    // produtos[indice] = produtos[tamArr]; //recebe o último elemento do array
+    // //Rotina que cria outro array de objetos tipo produtos
+    // let i = 0;
+    // while (i < tamArr) {
+    //   tmpArr[i] = produtos[i];
+    //   ++i;
+    // }
+
+    /**
+     * Alternativa pra excluir sem mudar a ordem dos produtos:
+     */
     let i = 0;
-    while (i < tamArr) {
-      tmpArr[i] = produtos[i];
-      ++i;
+    while(i < produtos.length) { // uso o tamanho original
+      if (i !== indice) { // adiciona somente se não for o produto deletado
+        tmpArr.push(produtos[i]);
+      }
+      i++;
     }
 
     produtos = tmpArr;
@@ -176,6 +251,10 @@ function excluir(indice) {
   listarTabelaDinamica();
 }
 
+// faltou validação dos inputs.
+// Não tinha regra pré-definida pra validação,
+// então bastava evitar nomes/descrições/valores
+// vazios ou algo assim
 function incluirProdutoAPartirDoFormulario() {
   //para salvar e atualizar
   nome = document.querySelector('#nome').value;
